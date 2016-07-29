4e51af9b19bf30b07f1874591b421addc91add4c
zhangdebao <zhangdebao@autohome.com.cn>
2016-07-29 18:47:29 +0800

    一些细节

--- app/src/main/java/db/keep/MainActivity.java
+++ app/src/main/java/db/keep/MainActivity.java
@@ -1,13 +1,65 @@
 package db.keep;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import db.keep.app.KeepApplication;
+import db.keep.common.BaseActivity;
+import db.keep.db.DbHelper;
+import db.keep.utils.ToastUtils;
+
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+    private double exitTime = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        findViewById(R.id.b1).setOnClickListener(this);
+        findViewById(R.id.b2).setOnClickListener(this);
+        findViewById(R.id.b3).setOnClickListener(this);
+        findViewById(R.id.b4).setOnClickListener(this);
+        findViewById(R.id.b5).setOnClickListener(this);
+        DbHelper.getDbHelperInstance().getWritableDatabase();
+    }
+
+    @Override
+    public int getBodyView() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.b1:
+                ToastUtils.showMsg("测试一");
+                break;
+            case R.id.b2:
+                ToastUtils.showMsg("测试二");
+                break;
+            case R.id.b3:
+                ToastUtils.showMsg("测试三");
+                break;
+            case R.id.b4:
+                ToastUtils.showMsg("测试四");
+                break;
+            case R.id.b5:
+                ToastUtils.showMsg("测试五");
+                break;
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (System.currentTimeMillis() - exitTime < 2000) {
+                KeepApplication.getInstance().exit();
+            } else {
+                ToastUtils.showMsg("再按一次退出程序");
+                exitTime = System.currentTimeMillis();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
--- app/src/main/java/db/keep/db/DbHelper.java
+++ app/src/main/java/db/keep/db/DbHelper.java
@@ -5,6 +5,8 @@ import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import db.keep.app.KeepApplication;
+
 /**
  * Created by zdb on 2016/7/28.
  * 数据库版本管理
@@ -12,7 +14,20 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DbHelper extends SQLiteOpenHelper {
 
     public static final String databaseName = "keep";
-    public static final int databaseVersion = 4;
+    public static final int databaseVersion = 2;
+
+    private static DbHelper mDbHelper = null;
+
+    public static DbHelper getDbHelperInstance() {
+        if (mDbHelper == null) {
+            mDbHelper = new DbHelper(KeepApplication.getInstance().getApplicationContext());
+        }
+        return mDbHelper;
+    }
+
+    public static void initDbHelper(Context context) {
+        mDbHelper = new DbHelper(context);
+    }
 
 
     public DbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
@@ -29,8 +44,8 @@ public class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-      /*  String create_db_sql = "";
-        db.execSQL(create_db_sql);*/
+        String create_db_sql = "CREATE TABLE \"record\" (\"_id\" integer NOT NULL PRIMARY KEY AUTOINCREMENT, \"money\" text, \"data\" data, \"cause\" text" + ");";
+        db.execSQL(create_db_sql);
     }
 
     @Override
